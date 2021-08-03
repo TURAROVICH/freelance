5f39311bb12c1328304e0282f0d2f0904e5d6b60
TURAROVICH <turaaa555@gmail.com>
2021-08-03 22:36:43 +0600

    first commit

---
+++ components/Modal.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="container-main">
+    <div class="main">
+        <div class="container">
+            <slot/>
+        </div>
+        <img @click="$emit('close')" src="../assets/imgs/close.svg" alt="">
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.container-main{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba($color: #3e97e4, $alpha: 0.8);
+    
+position: fixed;
+top: 0;
+left: 0;
+z-index: 5001;
+width: 100%;
+height: 100vh;
+overflow-x: scroll;
+/* width */
+&::-webkit-scrollbar {
+   display: none;
+
+}
+
+/* Track */
+&::-webkit-scrollbar-track {
+  display: none;
+}
+
+/* Handle */
+&::-webkit-scrollbar-thumb {
+   display: none;
+
+}
+
+/* Handle on hover */
+&::-webkit-scrollbar-thumb:hover {
+   display: none;
+
+}
+}
+.main{
+    margin: 500px 0  100px 0;
+    width: 60%;
+    padding: 50px 80px;
+    position: relative;
+background: #F7F9FF;
+box-shadow: -4px 4px 15px rgba(49, 69, 106, 0.2), 1px 1px 25px rgba(136, 165, 191, 0.15);
+border-radius: 6px;
+    img{
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,653 @@
+<template>
+  <div class="main">
+     <div class="wrapper">
+          <div class="welcome-part">
+            <img src="../assets/imgs/welcome-part.png" alt="">
+            <img src="../assets/imgs/ura.png" class="abs" alt="">
+            <div class="content">
+              <img src="../assets/imgs/el.png" alt="">
+              <div class="title">
+                Организуй праздник сам!
+              </div>
+              <div class="des">
+                Найди проверенных людей для своего праздника
+              </div>
+
+              <div class="btns">
+                <div class="btn"><span>СОЗДАТЬ ЗАДАНИЕ</span></div>
+                <div class="link"><nuxt-link to="#">Смотреть предложения</nuxt-link></div>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="about-part">
+              <div class="title">
+Еasyfest дает возможность сэкономить время 
+для организации Вашего незабываемого праздника
+              </div>
+              <div class="des">
+Для Вас мы стараемся  тщательно отбирать надежных и проверенных исполнителей. Ежедневно отслеживая недобросовестных и некомпетентных поставщиков услуг.  Нам Вы можете легко доверить любой Ваш праздник.
+              </div>
+          </div>
+
+          <div class="spezialition">
+            <img src="../assets/imgs/spez.svg" alt="">
+
+
+            <div class="container">
+                <div class="title">Специализации</div>
+
+                <div class="links">
+                    <div class="link">
+                      <div class="title">Декор, инвентарь </div>
+                    <ul>
+                      <li>Шарики </li>
+                      <li>Флористика </li>
+                      <li>Декор </li>
+                      <li>Транспорт</li>
+                    </ul>
+                    </div>
+
+                    <div class="link">
+                      <div class="title">Персонал  </div>
+                    <ul>
+                      <li>Ведущие  </li>
+                      <li>Аниматоры  </li>
+                      <li>Фото/видеосъемка  </li>
+                      <li>Артисты</li>
+                       <li>Стилисты</li>
+                    </ul>
+                    </div>
+
+
+                    <div class="link">
+                      <div class="title">Площадки </div>
+                    <ul>
+                      <li>Рестораны  </li>
+                      <li>Кафе </li>
+                      <li>Банкетные залы  </li>
+                      <li>Кейтеринг</li>
+                      <li>Детские площадки</li>
+                    </ul>
+                    </div>
+
+                    <div class="link">
+                      <div class="title">Праздник под ключ  </div>
+                    <ul>
+                      <li>Праздник под ключ </li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+
+          <div class="with">
+            <div class="content">
+              <div class="title">С EasyFast</div>
+               <div class="des">
+                Вы не только экономите время. 
+                Вы можете начать работать 
+                и стать нашим проверенным исполнителем
+                </div>
+             <nuxt-link to="#"> <div class="btn"><span>СТАТЬ ИСПОЛНИТЕЛЕМ</span></div></nuxt-link>
+            </div>
+            <img src="../assets/imgs/ef.png" alt="">
+          </div>
+
+        <div class="snippet s-1">
+          <img src="../assets/imgs/s-1.svg" alt="">
+
+          <div class="content">
+            <div class="m-title">Как это работает?</div>
+            <div class="title">1.Этап </div>
+            <div class="des">Зарегистрируйтесь на нашем сайте пройдя первоначальную проверку</div>
+          </div>
+        </div>
+
+        <div class="snippet s-2">
+          <img src="../assets/imgs/s-2.svg" alt="">
+
+          <div class="content">
+            <div class="title">2.Этап </div>
+            <div class="des">дождитесь письма подтверждения
+от нас </div>
+          </div>
+        </div>
+
+  <div class="snippet s-3">
+          <img src="../assets/imgs/s-3.svg" alt="">
+
+          <div class="content">
+            <div class="title">3.Этап </div>
+            <div class="des">Станьте нашим партнером и начинайте зарабатывать </div>
+            <div class="btn"><span>ПОСМОТРЕТЬ ЗАДАНИЯ</span></div>
+          </div>
+        </div>
+
+
+
+      <div class="advantages">
+        <div class="title">Преимущества</div>
+        <div class="advantages-cards">
+          <div class="advantages-card">
+            <img src="../assets/imgs/a-1.svg" alt="">
+            <div class="text">Вы можете самостоятельно организовать Ваш праздник без лишних затрат</div>
+          </div>
+          <div class="advantages-card">
+            <img src="../assets/imgs/a-2.svg" alt="">
+            <div class="text">Вы можете самостоятельно организовать Ваш праздник без лишних затрат</div>
+          </div>
+          <div class="advantages-card">
+            <img src="../assets/imgs/a-3.svg" alt="">
+            <div class="text">Вы можете самостоятельно организовать Ваш праздник без лишних затрат</div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="reviews">
+          <div class="title">Отзывы</div>
+
+            <div class="slider-container">
+              <div class="slide">
+                  <div class="slide-head">
+                    <div class="profile-data">
+                      <img src="../assets/imgs/review.svg" alt="">
+                      <div class="review-data">
+                        <div class="full-name">
+                          Игорь Р.
+                        </div>
+                        <div class="data">
+                          <span>Отзывы:</span>
+                                <div>
+                                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.23791 5.2168H1.23901C0.963039 5.2168 0.739273 5.43883 0.739273 5.71273V13.6475C0.739273 13.9213 0.963006 14.1434 1.23901 14.1434H3.23787C3.51384 14.1434 3.73761 13.9214 3.73761 13.6475V5.7127C3.73761 5.43883 3.51391 5.2168 3.23791 5.2168Z" fill="#667CA6"/>
+                                <path d="M15.4772 6.53166C15.389 5.76234 14.6649 5.21707 13.8846 5.21707H9.98846C10.3196 4.62871 10.4973 2.96459 10.4894 2.2848C10.4763 1.15954 9.53731 0.257812 8.40335 0.257812H7.99098C7.71476 0.257812 7.49122 0.479616 7.49122 0.753752C7.49122 1.90055 7.04123 3.97049 6.19257 4.8127C5.62136 5.37955 5.13303 5.58495 4.49272 5.90251V13.3438C5.47306 13.6681 6.71777 14.1438 8.61496 14.1438H11.8841C12.9612 14.1438 13.8001 13.1538 13.3827 12.1078C14.0187 11.9359 14.4876 11.3571 14.4876 10.6723C14.4876 10.479 14.4501 10.294 14.3822 10.124C15.4539 9.83429 15.8483 8.52119 15.1045 7.69671C15.3778 7.39376 15.5285 6.97939 15.4772 6.53166Z" fill="#667CA6"/>
+                                </svg>
+                                <span>5</span>
+                                </div>
+
+                                <div>
+                                  <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.6155 9.15039L13.6145 9.15039C13.8905 9.15039 14.1143 8.92836 14.1143 8.65445L14.1143 0.71972C14.1143 0.445848 13.8905 0.223783 13.6145 0.223783L11.6155 0.223783C11.3395 0.223783 11.1157 0.445815 11.1157 0.71972L11.1157 8.65449C11.1157 8.92836 11.3395 9.15039 11.6155 9.15039Z" fill="#667CA6"/>
+                                <path d="M0.374893 7.83553C0.455027 8.60485 1.11336 9.15012 1.82266 9.15012L5.36461 9.15012C5.0636 9.73848 4.90203 11.4026 4.9092 12.0824C4.92108 13.2076 5.77474 14.1094 6.8056 14.1094L7.18049 14.1094C7.43159 14.1094 7.63481 13.8876 7.63481 13.6134C7.63481 12.4666 8.04389 10.3967 8.81539 9.55449C9.33468 8.98764 9.7786 8.78224 10.3607 8.46468L10.3607 1.02334C9.46949 0.699087 8.33794 0.223442 6.61323 0.223442L3.64135 0.223442C2.66212 0.223442 1.89947 1.21344 2.27892 2.25943C1.70081 2.43134 1.27447 3.0101 1.27447 3.69492C1.27447 3.88818 1.30863 4.07316 1.3703 4.24316C0.396083 4.5329 0.0374899 5.846 0.713656 6.67049C0.465244 6.97343 0.328251 7.3878 0.374893 7.83553Z" fill="#667CA6"/>
+                                </svg>
+                                <span>1</span>
+                                </div>
+                                <div>
+                                  <span>Рейтинг: </span> <i class="bi-star-fill" style="color:#EFBA03 "></i>
+                                </div>
+
+
+                        </div>
+                      </div>
+                    </div>
+                    <div class="done">
+                      <div class="title">Выполнено заданий:</div><span>5</span>
+                    </div>
+                  </div>
+                  <div class="line">
+
+                  </div>
+
+                  <div class="slide-footer">
+                    Не следует, однако забывать, что постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. Задача организации, 
+в особенности же начало повседневной работы по формированию позиции представляет собой интересный эксперимент проверки соответствующий условий активизации...
+                  </div>
+
+              </div>
+            </div>
+
+      </div>
+
+
+      <div class="news">
+        <div class="title">Новости</div>
+
+        
+      <div class="cards">
+
+        <div class="grid-1">
+            <div @click="show=true" class="card-1">
+              <div class="wrapper">
+                <div class="title">
+              Текст новости
+                </div>
+                <div class="des">
+    Не следует, однако забывать, что постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. Задача организации, 
+    в особенности же начало повседневной ...
+                </div>
+                </div>
+            </div>
+             <div class="card-2">
+            <img src="../assets/imgs/joke-1.png" alt="">
+          </div>
+          </div>
+            <div class="grid-2">
+              <div class="card-2">
+            <img src="../assets/imgs/joke-1.png" alt="">
+          </div>
+          <div class="card-1">
+            <img src="../assets/imgs/joke.png" alt="">
+          </div>
+          </div>
+          </div>
+
+        
+
+      </div>
+     </div>
+       <Modal @close="show=false" v-if="show">
+            <img src="../assets/imgs/joke.png" width="100%" style="position:static;" alt="">
+           <div class="title-modal">
+              Текст новости
+                </div>
+                <div class="des-container">
+                <div class="des-modal">
+    Не следует, однако забывать, что постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. Задача организации, 
+    в особенности же начало повседневной ...
+                </div>
+                  <div class="des-modal">
+    Не следует, однако забывать, что постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание системы обучения кадров, соответствует насущным потребностям. Задача организации, 
+    в особенности же начало повседневной ...
+                </div>
+                </div>
+       </Modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data:()=>({
+    show:false
+  })
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css");
+@import "../assets/styles/main.scss";
+
+.center{
+  @include center;
+}
+.title-modal{
+  @include h1;
+  margin: 30px 0;
+}
+.des-container{
+  display: flex;
+  flex-direction: column;
+  gap:20px;
+}
+
+
+
+
+
+.news{
+  margin: 150px 0 ;
+  @include center{
+    flex-direction: column;
+  }
+  .title{
+    @include h1{
+      margin: 0 0 40px 0;
+    }
+  }
+  .cards{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    .grid-1{
+      cursor: pointer;
+      display: grid;
+      grid-template-columns: 1.5fr 1fr;
+      grid-auto-rows: 1fr;
+    }
+    .grid-2{
+      display: grid;
+      grid-template-columns: 1fr 1.5fr;
+      grid-auto-rows: 1fr;
+      grid-gap: 30px;
+      .card-1{
+        width: 96%;
+      }
+    }
+    .card-1{
+      width: 436px;
+      background: url("../assets/imgs/joke.png") no-repeat;
+      backdrop-filter: blur(10px);
+      background-size: cover;
+      background-position: center ;
+      .wrapper{
+       width: 100%;
+       height: 100%;
+       padding: 48px 46px;
+      background: rgba(255, 224, 128, 0.8);
+      }
+      .title{
+        @include h2;
+      }
+    }
+  }
+
+
+}
+.reviews{
+  margin-top: 150px;
+  @include center{
+    flex-direction: column;
+    gap: 40px;
+  }
+  .title{
+    @include h1;
+  }
+.slider-container{
+  width: 700px;
+  min-height: 185px;
+  .slide{
+    width: 100%;
+    height: 100%;
+
+
+     .slide-footer{
+    width: 100%;
+    @include h4;
+  }
+
+
+  .data {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+   & > span{
+    @include h4;
+    color:$tg;
+
+  }
+  }
+  .done{
+    display: flex;
+    .title{
+      @include h4;
+      color: $tg;
+
+    }
+
+    span{
+      @include h4;
+    }
+  }
+  
+  .line{
+    height: 1px;
+    width: 100%;
+    background: #D6E4F0;
+    margin: 10px 0;
+  }
+
+  .slide-head{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+  }
+  .profile-data{
+    display: flex;
+   align-items: center;
+   gap: 20px;
+   .full-name{
+     @include h3;
+   }
+  }
+
+}
+}
+ 
+}
+
+
+
+.advantages{
+  margin-top: 100px;
+  @include center{
+    flex-direction: column;
+    gap: 40px;
+  }
+  .advantages-cards{
+    @include center{
+      gap:30px;
+    }
+  }
+  .advantages-card{
+    @include center{
+      flex-direction: column;
+      gap: 15px;
+    }
+  }
+  .title{
+    @include h1;
+  }
+  .text{
+    @include h4{
+      width: 50%;
+      text-align: center;
+    }
+  }
+}
+
+
+
+
+.s-3{
+  margin-top: 60px;
+}
+.snippet{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 230px;
+  .content{
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 311px;
+  }
+  .m-title{
+    @include h1;
+  }
+  .title{
+    @include h2;
+  }
+  .des{
+    @include h4;
+  }
+}
+
+.s-2{
+  flex-direction: row-reverse;
+}
+
+
+
+.with{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  a{
+    text-decoration: none;
+  }
+  .content{
+    width: 45%;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    }
+  .title{
+    @include h1;
+  }
+  .des{
+    @include h4;
+  }
+  .btn{
+    background: linear-gradient(215.31deg, #F9F0C3 9.73%, #EFBB03 71.72%);
+    border-radius: 100px;
+    width: 246px;
+    height: 43px;
+    @include center;
+    span{
+      @include h3;
+    }
+  }
+}
+
+.spezialition{
+  padding: 90px 0;
+  & > img{
+    margin-left: 30%;
+  }
+
+  .container{
+   
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap :30px;
+    justify-content: center;
+    align-items: center;
+
+    &>.title{
+      @include h1;
+      margin: 71px 0;
+    }
+
+    .links{
+        display: flex;
+        justify-content: space-between;
+        gap: 52px;
+      .link{
+
+        display: flex;
+        flex-direction: column;
+        gap:25px;
+
+        &>.title{
+          @include h3;
+        }
+      }
+      ul li{
+        list-style: none;
+        @include h4;
+        color: $tg;
+      }
+      ul{
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+      }
+    }
+  }
+}
+
+
+.about-part{
+  width: 85%;
+  margin-top: 200px;
+  padding: 0 0 0 200px;
+  display: flex;
+  flex-direction: column;
+  gap:30px;
+  .title{
+    @include h2;
+  }
+  .des{
+    @include h3;
+  }
+}
+
+
+
+
+  .welcome-part{
+    position: relative;
+    display: flex;
+    justify-content: center;
+    gap:100px;
+    .abs{
+      position: absolute; 
+      top: -80px;
+      left: 0;
+      z-index: 10;
+    }
+
+    .content{
+      @include center{
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        gap:30px;
+
+
+      }
+      .title{
+        @include h1;
+      }
+      .des{
+        @include h2;
+      }
+
+      .btns{
+        display: flex;
+        align-items: flex-end;
+        justify-content: flex-end;
+        align-self: flex-end;
+        justify-self: flex-end;
+        flex-direction: column;
+        gap :20px;
+        .link a{
+          color: $tg;
+        }
+        .btn{
+        width: 196px;
+        height: 42px;
+        background: linear-gradient(215.31deg, #F9F0C3 9.73%, #EFBB03 71.72%);
+        border-radius: 100px;
+        @include center;
+        span{
+          @include h3{
+            font-weight: 500;
+          }
+        }
+        }
+      }
+    }
+  }
+
+
+.main{
+  width: 100%;
+  background:
+  url("../assets/imgs/bg1.png") no-repeat,
+  url("../assets/imgs/bg2.png") no-repeat,
+  url("../assets/imgs/bg.png") no-repeat,$main-bg;
+ background-size: 100% auto;
+ background-position: 0 0, 0 250px,0 1450px;
+  display: flex;
+  justify-content: center;
+  
+
+  .wrapper{
+    width: 90%;
+    padding: 30px 0;
+  }
+}
+</style>
+
+
